ffd03dd58b48acd3e2910d1dedaacedc20bee26f
Tatjana Danchenko <tatjana_danchenko@epam.com>
2023-04-20 14:27:31 +0300

    first

---
+++ UnitTests/test/testUnit.js
@@ -0,0 +1,26 @@
+const NumbersValidator = require('./NumbersValidator');
+const { expect } = require('chai');
+
+describe("Unit tests for NumberValidator", () => {
+  let numbersValidator;
+
+  beforeEach(() => {
+    numbersValidator = new NumbersValidator();
+  });
+
+      
+  describe("verify is numberEven", function() {
+    test("Even number", () => {
+      expect(numbersValidator.isNumberEven(8)).toBe(true);
+      //expect(nv.isNumberEven(4)).to.be.true;
+    });
+    it('should return true when passed an even number', function() {
+      expect(numbersValidator.isNumberEven(8)).toBe(true);
+    });
+
+    it("notEven number", () => {
+      expect(numbersValidator.isNumberEven(8)).toBe(`[${number}] is not of type "Number" it is of type "${typeOfVariable}"`);
+      });
+    });
+});
+
